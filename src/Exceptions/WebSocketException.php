cb2c1a99ac6001b69a695d70946c1376b0eb3b6e
Ilyas <ilyas@ody.dev>
2025-03-23 16:28:05 +0000

    add publish config files

--- src/Exceptions/WebSocketException.php
+++ src/Exceptions/WebSocketException.php
@@ -1,11 +1,4 @@
 <?php
-/*
- *  This file is part of ODY framework.
- *
- *  @link     https://ody.dev
- *  @document https://ody.dev/docs
- *  @license  https://github.com/ody-dev/ody-foundation/blob/master/LICENSE
- */
 
 namespace Ody\Websocket\Exceptions;
 
