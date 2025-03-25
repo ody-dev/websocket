a53efdaa477af7e55fb19661ff0257a6f5b1fd55
Ilyas <ilyas@ody.dev>
2025-03-25 17:33:05 +0000

    Merge pull request #1 from ody-dev/develop

--- src/OdyWebsocketClient.js
+++ src/OdyWebsocketClient.js
@@ -1,3 +1,11 @@
+/*
+ *  This file is part of ODY framework.
+ *
+ *  @link     https://ody.dev
+ *  @document https://ody.dev/docs
+ *  @license  https://github.com/ody-dev/ody-foundation/blob/master/LICENSE
+ */
+
 /**
  * Ody WebSocket Client
  * A simple client for the Ody WebSocket server with channel support
